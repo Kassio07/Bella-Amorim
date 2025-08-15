0e116f2492360b1f87ecdd0d6e5cb621ca48d609
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-08-15 20:59:34 -0300

    Criação da seção de beneficios

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// simplificar para selecionar elementos
+let c = (e) => document.querySelector(e);
+let cs = (e) => document.querySelectorAll(e)
+
+// Controles
+const textoInsert = c(".textoDigitado");
+
+// Destaque no texto - maquina de escrever
+function textoEscrever(){
+ for(let i = 0; i < textoInsert.length; i++){
+  textoInsert.innerHTML += textoInsert.charAt(i);
+  setTimeout(textoEscrever, 100);
+ }
+}
+
+textoEscrever();
