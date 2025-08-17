4f8c7156ab0892595f044476d90c583ce84c011f
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-08-17 12:31:59 -0300

    Slider

--- js/script.js
+++ js/script.js
@@ -2,32 +2,36 @@
 let c = (e) => document.querySelector(e);
 let cs = (e) => document.querySelectorAll(e);
 
-let totalSlide = cs(".testemunhas-card").length; // Qtde de cards
-c(".slides").style.width = `calc(380 * ${totalSlide}px)`;
+let totalSlideItem = cs('.slider--item').length;
+let slideWidth = c('.slider--width').clientWidth;
+let widthItem = c('.slider--item').clientWidth;
+
+c('.slider--width').style.width = `${slideWidth * totalSlideItem}px`;
 
 let currentSlide = 0;
 
 function goPrev(){
  currentSlide --;
  if(currentSlide < 0){
-  currentSlide = totalSlide - 1
+  currentSlide = totalSlideItem -1;
  }
+
  updateMargin();
 }
 
 function goNext(){
  currentSlide ++;
- if(currentSlide > totalSlide - 1){
+ if(currentSlide > totalSlideItem - 1){
   currentSlide = 0;
  }
  updateMargin();
 }
 
-
 function updateMargin(){
- let slideWidth = c('.testemunhas-card').clientWidth;
- let newMargin = currentSlide * slideWidth;
- c('.slides').style.marginLeft = `-${newMargin}px`;
+c('.slider--width').style.transform = `translateX(-${currentSlide * widthItem}px)`;
 }
 
-setInterval(goNext, 4000);
+setInterval(goNext, 3000);
+
+
+
