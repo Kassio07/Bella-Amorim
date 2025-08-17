256d95f5b868671041c2ac295dea9f7cdb56e881
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-08-17 01:03:15 -0300

    testemunhas

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
-"use strict";
-
 // simplificar para selecionar elementos
 let c = (e) => document.querySelector(e);
-let cs = (e) => document.querySelectorAll(e)
+let cs = (e) => document.querySelectorAll(e);
 
-// Controles
-const textoInsert = c(".textoDigitado");
+let totalSlide = cs(".testemunhas-card").length; // Qtde de cards
+c(".slides").style.width = `calc(380 * ${totalSlide}px)`;
 
-// Destaque no texto - maquina de escrever
-function textoEscrever(){
- for(let i = 0; i < textoInsert.length; i++){
-  textoInsert.innerHTML += textoInsert.charAt(i);
-  setTimeout(textoEscrever, 100);
+let currentSlide = 0;
+
+function goPrev(){
+ currentSlide --;
+ if(currentSlide < 0){
+  currentSlide = totalSlide - 1
  }
+ updateMargin();
 }
 
-textoEscrever();
+function goNext(){
+ currentSlide ++;
+ if(currentSlide > totalSlide - 1){
+  currentSlide = 0;
+ }
+ updateMargin();
+}
+
+
+function updateMargin(){
+ let slideWidth = c('.testemunhas-card').clientWidth;
+ let newMargin = currentSlide * slideWidth;
+ c('.slides').style.marginLeft = `-${newMargin}px`;
+}
+
+setInterval(goNext, 4000);
