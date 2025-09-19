029d3969dbc89e5599664d4dde68d29bd8557509
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-09-18 23:10:08 -0300

    Criando o slide de imagens

--- js/script.js
+++ js/script.js
@@ -2,98 +2,103 @@
 let c = (e) => document.querySelector(e);
 let cs = (e) => document.querySelectorAll(e);
 
-// Slide auto Testemunhas-----------------------------------
-// Controles
-let totalSlides = cs(".slider--item"); // Pega todos os item
-let sliderWidthContainer = c(".slider--width"); // Pega o container dos item
-let slideWidthTotal = totalSlides.length; //Qtde total dos item 
-let widthItem = totalSlides[0].offsetWidth; //Largura de um item
-
-// Variavel de contator
+// Sldier site - controls
+let slider_container = c(".slider-container"); //Container principal
+let slideWidth = c(".slider--width"); //Faixa de slides
+let widthItemSlide = c(".slider--item").clientWidth; // largura do item
+let qtSlide_item = cs(".slider--item").length; // Qtde de item do slide
 let currentSlide = 0;
 
-// Função de navegação - Anterior
+// Ir para o anterior
 function goPrev() {
   currentSlide--;
   if (currentSlide < 0) {
-    currentSlide = slideWidthTotal - 1;
+    currentSlide = qtSlide_item - 1;
   }
-  updateMargin();
+  upDateMargin();
 }
 
-// Função de navegação - Próximo
+// ir para o proximo
 function goNext() {
   currentSlide++;
-  if (currentSlide > slideWidthTotal - 1) {
+  if (currentSlide >= qtSlide_item) {
     currentSlide = 0;
   }
-  updateMargin();
-}
-function updateMargin() {
-  sliderWidthContainer.style.transform = `translateX(-${currentSlide * widthItem}px)`;
+
+  upDateMargin();
 }
 
-// Pega a largura total da tela do usuário
-let larguraTela = window.innerWidth;
-// Roda essa função automatica somente quando a tela do usuário for maior que 577px
-if(larguraTela > 577){
-    setInterval(goNext, 3000);
+function upDateMargin() {
+  slideWidth.style.transform = `translateX(-${
+    currentSlide * widthItemSlide
+  }px)`;
 }
 
+let widthTela = window.innerWidth;
+if (widthTela > 768) {
+  setInterval(() => {
+    goNext();
+  }, 4000);
+} else {
+  // Slide touch - slide para dispositivos moveis no Touch --------------------------------------------------
+  // controls
+  let slides = cs(".slider--item");
+  let startX = 0;
+  let endX = 0;
+
+  function showSlide(index) {
+    // Não deixa voltar além do primeiro
+    if (index < 0) {
+      index = 0;
+    }
+    // Não deixa passar do último
+    if (index >= slides.length) {
+      index = slides.length - 1;
+    }
+    currentSlide = index;
+    slideWidth.style.transform = `translateX(${-widthItemSlide * index}px)`;
+  }
+
+  // Captura o INICIO do toque
+  slideWidth.addEventListener(
+    "touchstart",
+    (e) => {
+      startX = e.touches[0].clientX;
+    },
+    { passive: true }
+  );
+
+  // Captura o FIM do toque
+  slideWidth.addEventListener(
+    "touchend",
+    (e) => {
+      endX = e.changedTouches[0].clientX;
+      if (startX - endX > 50) {
+        // Desliza para esquerda -> Próximo
+        showSlide(currentSlide + 1);
+      } else if (endX - startX > 50) {
+        // Desliza para a direita -> anterior
+        showSlide(currentSlide - 1);
+      }
+    },
+    { passive: true }
+  );
+}
 
 // Arrow up to - Btn de voltar ao topo do site ----------------------------------------------
-
+const arrow = c(".arrow-to");
 window.addEventListener("scroll", () => {
-  const arrow = c(".arrow-to");
   // Esconde a seta no topo da página
   if (window.scrollY > 700) {
     arrow.style.display = "block";
   } else {
     arrow.style.display = "none";
   }
-  // Roda a função pra subir pro topo
-  arrow.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+});
+// função do btn pra subir pro topo do site
+arrow.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
   });
 });
-
-// Slide touch - slide para dispositivos moveis no Touch --------------------------------------------------
-// controls
-let slider = c(".slider--width");
-let slides = c(".slider--item");
-let startX = 0;
-let endX = 0;
-let currentIndex = 0;
-
-function showSlide(index) {
-  // Não deixa voltar além do primeiro
-  if (index < 0) {
-    index = 0;
-  }
-  // Não deixa passar do último
-  if (index >= slides.length) {
-    index = slides.length - 1;
-  }
-  currentIndex = index;
-  slider.style.transform = `translate(${-330 * index}px)`;
-}
-
-// Captura o INICIO do toque
-slider.addEventListener("touchstart", (e) => {
-  startX = e.touches[0].clientX;
-}, {passive: true});
-
-// Captura o FIM do toque
-slider.addEventListener("touchend", (e) => {
-  endX = e.changedTouches[0].clientX;
-  if (startX - endX > 50) {
-    // Desliza para esquerda -> Próximo
-    showSlide(currentIndex + 1);
-  } else if (endX - startX > 50) {
-    // Desliza para a direita -> anterior
-    showSlide(currentIndex - 1);
-  }
-}, {passive:true});
