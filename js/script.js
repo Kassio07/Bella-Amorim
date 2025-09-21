de10a33f21abb7f2279bae4fe1c014172e5f66d2
Kassio Kennedy <154475006+Kassio07@users.noreply.github.com>
2025-09-21 00:14:16 -0300

    Criando a função dos slides

--- js/script.js
+++ js/script.js
@@ -2,90 +2,56 @@
 let c = (e) => document.querySelector(e);
 let cs = (e) => document.querySelectorAll(e);
 
-// Sldier site - controls
-let slider_container = c(".slider-container"); //Container principal
-let slideWidth = c(".slider--width"); //Faixa de slides
-let widthItemSlide = c(".slider--item").clientWidth; // largura do item
-let qtSlide_item = cs(".slider--item").length; // Qtde de item do slide
-let currentSlide = 0;
+// Função dos Slides da - LP
+function showSlider(containerSelector, autoPlay = true) {
+  const container = c(containerSelector);
+  // Slide que contém todos os slides item
+  const slideMove = container.querySelector(".slider--width");
+  // Seleciona todos os itens do slide
+  const slides_item = container.querySelectorAll(".slider--item");
 
-// Ir para o anterior
-function goPrev() {
-  currentSlide--;
-  if (currentSlide < 0) {
-    currentSlide = qtSlide_item - 1;
-  }
-  upDateMargin();
-}
+  // Btn manual do slide
+  const next = container.querySelector(".next");
+  const prev = container.querySelector(".prev");
 
-// ir para o proximo
-function goNext() {
-  currentSlide++;
-  if (currentSlide >= qtSlide_item) {
-    currentSlide = 0;
-  }
-
-  upDateMargin();
-}
-
-function upDateMargin() {
-  slideWidth.style.transform = `translateX(-${
-    currentSlide * widthItemSlide
-  }px)`;
-}
-
-let widthTela = window.innerWidth;
-if (widthTela > 768) {
-  setInterval(() => {
-    goNext();
-  }, 4000);
-} else {
-  // Slide touch - slide para dispositivos moveis no Touch --------------------------------------------------
-  // controls
-  let slides = cs(".slider--item");
-  let startX = 0;
-  let endX = 0;
-
-  function showSlide(index) {
-    // Não deixa voltar além do primeiro
-    if (index < 0) {
-      index = 0;
+  // Variável de incremento
+  let current = 0;
+  // Largura do item do slide
+  let width_slide = slides_item[0].clientWidth;
+  // Lógica do slide
+  function vaPara(index) {
+    if (index <= 0) {
+      index = slides_item.length - 1;
     }
-    // Não deixa passar do último
-    if (index >= slides.length) {
-      index = slides.length - 1;
-    }
-    currentSlide = index;
-    slideWidth.style.transform = `translateX(${-widthItemSlide * index}px)`;
+    if (index >= slides_item.length) index = 0;
+
+    current = index;
+
+    slideMove.style.transform = `translateX(-${current * width_slide}px)`;
   }
-
-  // Captura o INICIO do toque
-  slideWidth.addEventListener(
-    "touchstart",
-    (e) => {
-      startX = e.touches[0].clientX;
-    },
-    { passive: true }
-  );
-
-  // Captura o FIM do toque
-  slideWidth.addEventListener(
-    "touchend",
-    (e) => {
-      endX = e.changedTouches[0].clientX;
-      if (startX - endX > 50) {
-        // Desliza para esquerda -> Próximo
-        showSlide(currentSlide + 1);
-      } else if (endX - startX > 50) {
-        // Desliza para a direita -> anterior
-        showSlide(currentSlide - 1);
-      }
-    },
-    { passive: true }
-  );
+  // Função dos botões manual do slides
+  function goPrev() {
+    vaPara(current - 1);
+  }
+  function goNext() {
+    vaPara(current + 1);
+  }
+  // Liga os botões manuais do slides
+  next.addEventListener("click", goNext);
+  prev.addEventListener("click", goPrev);
+  // AutoPlay
+  if (autoPlay && window.innerWidth > 768) {
+    setInterval(goNext, 3000);
+  }
+  return { goNext, goPrev, vaPara };
 }
 
-// Arrow up to - Btn de voltar ao topo do site ----------------------------------------------
+// Chama a função dos slides
+showSlider(".slider-container");
+showSlider(".slider-container.two");
+
+
+// Arrow up to - Btn de voltar ao topo do site 
 const arrow = c(".arrow-to");
 window.addEventListener("scroll", () => {
   // Esconde a seta no topo da página
